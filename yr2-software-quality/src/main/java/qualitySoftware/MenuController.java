2f44026c9c46f5a6be54f7b19020a03f62c34cea
Hakan Ege Özerol <hakan.ege.ozerol@student.nhlstenden.com>
2024-04-07 00:41:43 +0200

    Fix the undefined problem, added constructor FIX--

--- yr2-software-quality/src/main/java/qualitySoftware/MenuController.java
+++ yr2-software-quality/src/main/java/qualitySoftware/MenuController.java
@@ -11,7 +11,11 @@ import java.io.IOException;
 
 import javax.swing.JOptionPane;
 
-/** <p>The controller for the menu</p>
+/**
+ * <p>
+ * The controller for the menu
+ * </p>
+ * 
  * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
  * @version 1.1 2002/12/17 Gert Florijn
  * @version 1.2 2003/11/19 Sylvia Stuurman
@@ -21,12 +25,12 @@ import javax.swing.JOptionPane;
  * @version 1.6 2014/05/16 Sylvia Stuurman
  */
 public class MenuController extends MenuBar {
-	
+
 	private Frame parent; // the frame, only used as parent for the Dialogs
 	private Presentation presentation; // Commands are given to the presentation
-	
+
 	private static final long serialVersionUID = 227L;
-	
+
 	protected static final String ABOUT = "About";
 	protected static final String FILE = "File";
 	protected static final String EXIT = "Exit";
@@ -39,10 +43,10 @@ public class MenuController extends MenuBar {
 	protected static final String PREV = "Prev";
 	protected static final String SAVE = "Save";
 	protected static final String VIEW = "View";
-	
+
 	protected static final String TESTFILE = "test.xml";
 	protected static final String SAVEFILE = "dump.xml";
-	
+
 	protected static final String IOEX = "IO Exception: ";
 	protected static final String LOADERR = "Load Error";
 	protected static final String SAVEERR = "Save Error";
@@ -61,12 +65,12 @@ public class MenuController extends MenuBar {
 					xmlAccessor.loadFile(presentation, TESTFILE);
 					presentation.setSlideNumber(0);
 				} catch (IOException exc) {
-					JOptionPane.showMessageDialog(parent, IOEX + exc, 
-         			LOADERR, JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(parent, IOEX + exc,
+							LOADERR, JOptionPane.ERROR_MESSAGE);
 				}
 				parent.repaint();
 			}
-		} );
+		});
 		fileMenu.add(menuItem = mkMenuItem(NEW));
 		menuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {
@@ -81,7 +85,7 @@ public class MenuController extends MenuBar {
 				try {
 					xmlAccessor.saveFile(presentation, SAVEFILE);
 				} catch (IOException exc) {
-					JOptionPane.showMessageDialog(parent, IOEX + exc, 
+					JOptionPane.showMessageDialog(parent, IOEX + exc,
 							SAVEERR, JOptionPane.ERROR_MESSAGE);
 				}
 			}
@@ -110,7 +114,7 @@ public class MenuController extends MenuBar {
 		viewMenu.add(menuItem = mkMenuItem(GOTO));
 		menuItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {
-				String pageNumberStr = JOptionPane.showInputDialog((Object)PAGENR);
+				String pageNumberStr = JOptionPane.showInputDialog((Object) PAGENR);
 				int pageNumber = Integer.parseInt(pageNumberStr);
 				presentation.setSlideNumber(pageNumber - 1);
 			}
@@ -123,10 +127,10 @@ public class MenuController extends MenuBar {
 				AboutBox.show(parent);
 			}
 		});
-		setHelpMenu(helpMenu);		// needed for portability (Motif, etc.).
+		setHelpMenu(helpMenu); // needed for portability (Motif, etc.).
 	}
 
-// create a menu item
+	// create a menu item
 	public MenuItem mkMenuItem(String name) {
 		return new MenuItem(name, new MenuShortcut(name.charAt(0)));
 	}
--- yr2-software-quality/src/main/java/qualitySoftware/SlideViewerFrame.java
+++ yr2-software-quality/src/main/java/qualitySoftware/SlideViewerFrame.java
@@ -1,47 +1,54 @@
 package qualitySoftware;
+
 import java.awt.Dimension;
-import java.awt.event.WindowEvent;
 import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 /**
- * <p>The application window for a slideviewcomponent</p>
- * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
- * @version 1.1 2002/12/17 Gert Florijn
- * @version 1.2 2003/11/19 Sylvia Stuurman
- * @version 1.3 2004/08/17 Sylvia Stuurman
- * @version 1.4 2007/07/16 Sylvia Stuurman
- * @version 1.5 2010/03/03 Sylvia Stuurman
- * @version 1.6 2014/05/16 Sylvia Stuurman
-*/
-
+ * The application window for a slideviewcomponent
+ * 
+ * @author Ian F. Darwin, ian@darwinsys.com
+ * @author Gert Florijn
+ * @author Sylvia Stuurman
+ * @version 1.6 2014/05/16
+ */
 public class SlideViewerFrame extends JFrame {
-	private static final long serialVersionUID = 3227L;
-	
-	private static final String JABTITLE = "Jabberpoint 1.6 - OU";
-	public final static int WIDTH = 1200;
-	public final static int HEIGHT = 800;
-	
-	public SlideViewerFrame(String title, Presentation presentation) {
-		super(title);
-		SlideViewerComponent slideViewerComponent = new SlideViewerComponent(presentation, this);
-		presentation.setShowView(slideViewerComponent);
-		setupWindow(slideViewerComponent, presentation);
-	}
+    private static final long serialVersionUID = 3227L;
 
-// Setup GUI
-	public void setupWindow(SlideViewerComponent 
-			slideViewerComponent, Presentation presentation) {
-		setTitle(JABTITLE);
-		addWindowListener(new WindowAdapter() {
-				public void windowClosing(WindowEvent e) {
-					System.exit(0);
-				}
-			});
-		getContentPane().add(slideViewerComponent);
-		addKeyListener(presentation.getKeyController()); // add a controller
-		setMenuBar(new MenuController(this, presentation));	// add another controller
-		setSize(new Dimension(WIDTH, HEIGHT)); // Same sizes as Slide has.
-		setVisible(true);
-	}
+    private static final String TITLE = "Jabberpoint 1.6 - OU";
+    private static final int WIDTH = 1200;
+    private static final int HEIGHT = 800;
+
+    public SlideViewerFrame(String title, Presentation presentation) {
+        super(title);
+        SlideViewerComponent slideViewerComponent = new SlideViewerComponent(presentation, this);
+        presentation.setShowView(slideViewerComponent);
+        setupWindow(slideViewerComponent, presentation);
+    }
+
+    // Setup GUI
+    public void setupWindow(SlideViewerComponent slideViewerComponent, Presentation presentation) {
+        setTitle(TITLE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        getContentPane().add(slideViewerComponent);
+        addKeyListener(presentation.getKeyController()); // add a controller
+        setMenuBar(new MenuController(this, presentation)); // add another controller
+        setSize(new Dimension(WIDTH, HEIGHT)); // Same sizes as Slide has.
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        SwingUtilities.invokeLater(() -> {
+            Presentation presentation = new Presentation(); // Create your presentation object here
+            new SlideViewerFrame(TITLE, presentation);
+        });
+    }
 }
