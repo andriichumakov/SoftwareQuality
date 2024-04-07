c45ebe0fc213dd2f3d40526a80a3e5250f0018ba
Hakan Ege Özerol <hakan.ege.ozerol@student.nhlstenden.com>
2024-04-07 20:01:33 +0200

    removed everything a clean slate REMOVE--

--- yr2-software-quality/src/main/java/qualitySoftware/BitmapItemCreator.java
+++
@@ -1,9 +0,0 @@
-package qualitySoftware;
-
-import java.io.File;
-
-public class BitmapItemCreator {
-    public static BitmapItem createSlideItem(int level, String imageName) {
-        return new BitmapItem(level, imageName);
-    }
-}
