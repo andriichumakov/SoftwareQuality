2f11bac2546094c7205b722fce308eba20b89485
HakanEgeOzerol <111962975+HakanEgeOzerol@users.noreply.github.com>
2024-06-06 23:37:54 +0200

    Changed up the folder structure for testing

--- src/test/java/qualitySoftware/accessor/DemoPresentationTest.java
+++ src/test/java/qualitySoftware/accessor/DemoPresentationTest.java
@@ -1,4 +1,4 @@
-package qualitySoftware;
+package qualitySoftware.accessor;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- src/test/java/qualitySoftware/command/GotoSlideCommandTest.java
+++ src/test/java/qualitySoftware/command/GotoSlideCommandTest.java
@@ -1,4 +1,4 @@
-package qualitySoftware;
+package qualitySoftware.command;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
