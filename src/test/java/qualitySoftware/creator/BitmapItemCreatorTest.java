50c4aba4ad2b46f2861e3575718637e015813a9d
Hakan Ege Özerol <hakan.ege.ozerol@student.nhlstenden.com>
2024-06-07 18:30:44 +0200

    removed tests

--- src/test/java/qualitySoftware/creator/BitmapItemCreatorTest.java
+++
@@ -1,9 +0,0 @@
-package qualitySoftware.creator;
-
-import qualitySoftware.slide.BitmapItem;
-
-public class BitmapItemCreatorTest {
-    public static BitmapItem createSlideItem(int level, String imageName) {
-        return new BitmapItem(level, imageName);
-    }
-}
